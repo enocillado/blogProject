baa0c039e4c06034d2c2666839d6fa399420c050
enocillado <enocillado@starpay.com.ph>
2024-01-04 23:15:33 +0800

    save post

--- src/main/java/com/blog/blog/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blog.blog.repository;
-
-import com.blog.blog.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
